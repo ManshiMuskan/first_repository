7b628ff6af33dd94a13c50030935921c3fb28f93
ManshiMuskan <manshimuskan999@gmail.com>
2022-06-04 23:15:20 +0530

    Update P2_1.c

--- P2_1.c
+++ P2_1.c
@@ -4,10 +4,13 @@
 #include<float.h>
 int main(void)
 {
+//GitHub testing
 	printf("sizeof(char) = %u\n",sizeof(char));
 	printf("sizeof(short) = %u\n",sizeof(short));
 	printf("sizeof(int) = %u\n",sizeof(int));
 	printf("sizeof(long) = %u\n",sizeof(long));
+
+
 	printf("sizeof(float) = %u\n",sizeof(float));
 	printf("sizeof(double) = %u\n",sizeof(double));
 	printf("sizeof(long double) = %u\n",sizeof(long double));
